8d5ca76b284ef392ce482f10687018f01d070628
22120198 <trinhnguyenluong182306@gmail.com>
2024-12-20 12:30:31 +0700

    Feature activity typing of users

--- course_dave_gray/03_express_activity/public/app.js
+++ course_dave_gray/03_express_activity/public/app.js
@@ -1,20 +1,38 @@
 const socket = io('ws://localhost:3500');
 
+const msgInput = document.querySelector('input');
+const activity = document.querySelector('.activity');
+
 function sendMessage(e) {
     e.preventDefault()
-    const input = document.querySelector('input');
-    if (input.value) {
-        socket.emit('message', input.value);
-        input.value = "";
+    if (msgInput.value) {
+        socket.emit('message', msgInput.value);
+        msgInput.value = "";
     }
-    input.focus()
+    msgInput.focus()
 }
 
 document.querySelector('form')
     .addEventListener('submit', sendMessage)
 
 socket.on('message', (data) => {
+    activity.textContent = "";
     const li = document.createElement('li');
     li.textContent = data;
     document.querySelector('ul').appendChild(li);
+})
+
+msgInput.addEventListener('keypress',  () => {
+    socket.emit('activity', socket.id.substring(0, 5));
+})
+
+let activityTimer;
+socket.on('activity', (name) => {
+    activity.textContent = `${name} is typing...`;
+
+    // Clear after 3 seconds
+    clearTimeout(activityTimer);
+    activityTimer = setTimeout(() => {
+        activity.textContent = '';
+    }, 3000);
 })
